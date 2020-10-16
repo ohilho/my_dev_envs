1b871eda6ca8ed575b985192e0ecdef43696bc53
ohilho <iroiro515@gmail.com>
2020-10-16 18:37:42 +0900

    Fix entry.sh. read command in the shell script does not work with docker-compose

--- code-server/entry.sh
+++ code-server/entry.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 if [ -e /tmp/first_run ]; then
-    echo "Make a new password. If you don't want to use authentication, leave it empty and press enter."
-    echo "New password: "
-    read password
-    if [ -z ${password} ];then
-        sed "s/auth: password/auth: none/" /root/.config/code-server/config.yaml > /root/.config/code-server/config.yaml.new
-        rm /root/.config/code-server/config.yaml
-        mv /root/.config/code-server/config.yaml.new /root/.config/code-server/config.yaml
-        echo "No password input. Authentication disabled";
-    else
-        sed "s/password: 0000/password: ${password}/" /root/.config/code-server/config.yaml > /root/.config/code-server/config.yaml.new
-        rm /root/.config/code-server/config.yaml
-        mv /root/.config/code-server/config.yaml.new /root/.config/code-server/config.yaml
-        echo "Password set to \"${password}\""
-        rm /tmp/first_run
-    fi
+    echo "This is your FIRST time running this server."
+    echo "You SHOULD set your authentication option."
+    echo "you can find configuration file at:"
+    echo "    /root/.config/code-server/config.yaml"
+    rm /tmp/first_run
 fi
 code-server
 
